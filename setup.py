6b22122bd2642c8d4c2ac791ca8f657ab2a742df
Royendgel Silberie <royendgel@gmail.com>
2016-06-25 16:28:58 -0400

    removed open function in setupy.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 import setuptools
 
 setuptools.setup(name='m3u8-generator',
-                 version=1.0,
+                 version=1.1,
                  description='m3u8 playlist generator',
-                 long_description=open('README.md').read().strip(),
                  author='cambackup.com',
                  author_email='info@cambackup.com',
                  url='https://github.com/cambackup/m3u8-generator',
